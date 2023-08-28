f54dd39944317f04f549eb5cff70e5ab793b2f5c
Ezequiel <ezequielgiribarren@gmail.com>
2023-08-28 17:05:33 -0300

    servicio domicilio

--- src/Components/Servicios/Servicios.jsx
+++ src/Components/Servicios/Servicios.jsx
@@ -70,6 +70,32 @@ const Servicios = () => {
           {/* Resto del contenido */}
         </div>
       </div>
+
+      <div className='p-5 down-services'>
+          <div className='domicilio'>
+          <div className='up-domicilio'>
+            <div className='title-domicilio'>
+              <h3>Nuevo servicio</h3>
+            </div>
+          </div>
+          <div className='description-domicilio'>
+            <div className='img-domicilio'><img src="/images/servicios/domicilio.png" alt="" /></div>
+            <img className='img-mobile-domicilio' src="/images/servicios/mobile-domicilio.png" alt="" />
+            <div className='medio-domicilio'>
+              <h4>¡Pedí tu Consulta Cardiológica a Domicilio!</h4>
+              <p className='p-mobile-domicilio'>Priorizamos tu comodidad y bienestar. Consultá por nuestras zonas de cobertura y lleva la atención cardiológica especializada directamente a tu puerta.</p>
+              <a href="https://wa.me/+541164977403?text=Hola,%20solicito%20información%20sobre%20consulta%20cardiológica%20a%20domicilio"><button className='button'>Consultar</button></a>
+            </div>
+            <div className='p-domicilio none-domicilio'><p>Priorizamos tu comodidad y bienestar. Consultá por nuestras zonas de cobertura y lleva la atención cardiológica especializada directamente a tu puerta. </p></div>
+          </div>
+
+        </div>
+        <div className='contact-services'>
+          <h2 className='info-services-title'>¿Querés más información de nuestros servicios?</h2>
+          <a href="https://wa.me/+541164977403" target='_blank'> <button className='button3'>Contanos</button></a>
+        </div>
+        </div>
+
     </section>
   );
 };
